1f0f447c3d24c40d6fb5dc51338c2eba2ba6361d
Asankhaya Sharma <asankhaya@Asankhayas-MacBook-Pro.local>
2019-08-06 14:30:49 +0800

    Remove the toc.sh, we will edit the README directly

--- toc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-title="$(grep title: metadata.yaml | sed -e 's/title: //' -e "s/'//g")"
-
-echo "# $title"
-
-paste -d '\0' \
-  <(ls chapters | sort | sed -e 's/\(.*\).md/- [\1]/') \
-  <(ls chapters | sort | sed -e 's!\(.*\).md!(chapters/\1.md)!' -e 's/ /%20/g')
